e292da8c000aa33de61ecca30e1b3de623345738
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-02-26 19:24:09 +0000

    update tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,10 @@
 import { GatewayEzMode } from ".";
+import { Account } from "./account";
+
+
+const RATTA_RESOURCE = "resource_rdx1t5wuuwfg3uk2y5l88rya553t3v4zyepf7wjekphryw6yepedyf7pug"
+const SOME_RANDOM_ACCOUNT = "account_rdx1cx26ckdep9t0lut3qaz3q8cj9wey3tdee0rdxhc5f0nce64lw5gt70"
+const DELETED_RATTA_ACCOUNT = "account_rdx12xru8aww4w73rr7r3wrc9nk428jr93efxvwtazen7hs3u43cyl5ssv"
 
 describe('gatewaySdk', () => {
     it('should initialize correctly', () => {
@@ -11,26 +17,30 @@ describe('gatewaySdk', () => {
     });
 
     it('should not error when I ask for an accounts balances', async () => {
-        const account = "account_rdx1cx26ckdep9t0lut3qaz3q8cj9wey3tdee0rdxhc5f0nce64lw5gt70"
         const gateway = new GatewayEzMode
-        const balances = await gateway.getAllFungibleAccountBalances(account)
+        const account = gateway.getAccount(SOME_RANDOM_ACCOUNT)
+        const balances = await account.getFungibleBalances()
         console.log(balances)
         expect(balances).toBeDefined()
     });
 
     // I put 1 RÅTTA in this account and then deleted access to it on-ledger so this test should always pass
     it('should see 1 ratta in this account', async () => {
-        const account = "account_rdx12xru8aww4w73rr7r3wrc9nk428jr93efxvwtazen7hs3u43cyl5ssv"
-        const rattaResourceAddress = "resource_rdx1t5wuuwfg3uk2y5l88rya553t3v4zyepf7wjekphryw6yepedyf7pug"
         const gateway = new GatewayEzMode
-        const balances = await gateway.getAllFungibleAccountBalances(account)
+        const account = gateway.getAccount(DELETED_RATTA_ACCOUNT)
+        const balances = await account.getFungibleBalances()
         expect(balances).toBeDefined()
-        expect(balances.length).toBe(1)
-        const rattaBalance = balances.find((balance) => balance.token.resourceAddress === rattaResourceAddress)
+        const rattaBalance = balances.find((balance) => balance.token.resourceAddress === RATTA_RESOURCE)
         if (!rattaBalance) {
             throw new Error("No ratta balance found")
         }
         expect(rattaBalance.balance).toBe("1")
-        expect(rattaBalance.token.resourceAddress).toBe("resource_rdx1t5wuuwfg3uk2y5l88rya553t3v4zyepf7wjekphryw6yepedyf7pug")
+        expect(rattaBalance.token.resourceAddress).toBe(RATTA_RESOURCE)
+    });
+
+    // use the account class on its own with a regular gateway instance
+    it('should be able to get the gateways status', async () => {
+        const account = new Account(SOME_RANDOM_ACCOUNT)
+        await account.getFungibleBalances()
     });
 });
