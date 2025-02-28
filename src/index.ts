c0352af40c995687cff7f14b65a61d833169dcf0
Alex Stelea <alex@stelea.io>
2025-02-28 10:53:04 +0000

    feat: add pollTransactionStatus method

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
-import { GatewayApiClient } from "@radixdlt/babylon-gateway-api-sdk"
-import { Account } from "./account"
-
+import {
+  GatewayApiClient,
+  TransactionStatusResponse,
+} from "@radixdlt/babylon-gateway-api-sdk";
+import { Account } from "./account";
+import {
+  PollTransactionStatusOptions,
+  pollTransactionStatus,
+} from "./pollTransactionStatus";
 
 // Please add methods to this class to extend the functionality
 export class GatewayEzMode {
-    gateway: GatewayApiClient
-    constructor(gateway?: GatewayApiClient) {
-        if (gateway) {
-            this.gateway = gateway
-        } else {
-            this.gateway = GatewayApiClient.initialize({
-                applicationName: '',
-                networkId: 1,
-            })
-        }
+  gateway: GatewayApiClient;
+  constructor(gateway?: GatewayApiClient) {
+    if (gateway) {
+      this.gateway = gateway;
+    } else {
+      this.gateway = GatewayApiClient.initialize({
+        applicationName: "",
+        networkId: 1,
+      });
     }
+  }
 
-    getAccount(address: string): Account {
-        return new Account(address, this.gateway)
-    }
+  getAccount(address: string): Account {
+    return new Account(address, this.gateway);
+  }
+
+  pollTransactionStatus(
+    transactionId: string,
+    options?: Omit<PollTransactionStatusOptions, "gatewayApiClient">
+  ): Promise<TransactionStatusResponse> {
+    return pollTransactionStatus(transactionId, {
+      ...(options || {}),
+      gatewayApiClient: this.gateway,
+    });
+  }
 }
