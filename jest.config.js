ba703c3c4c1f6f67a5a7faa50b8029bd76d2ae15
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-02 16:06:51 +0000

    remove jest config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/'],
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: ['src/**/*.ts'],
-};
