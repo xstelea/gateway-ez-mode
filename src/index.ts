301279c5ba099eb4ac5b6626904911f7c34ee487
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-11 16:09:27 +0000

    improve documentation of some parts of the code and format some files

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,21 @@ import {
     pollTransactionStatus,
 } from './pollTransactionStatus';
 
-// Please add methods to this class to extend the functionality
+/**
+ * A wrapper around the GatewayApiClient that provides
+ * a more user-friendly interface for common tasks.
+ */
 export class GatewayEzMode {
+    /**
+     * The GatewayApiClient instance used for API calls.
+     * @remarks It's public, so you can use it if you need more advanced gateway usage.
+     */
     gateway: GatewayApiClient;
+
+    /**
+     * Creates a new GatewayEzMode instance.
+     * @param gateway Optional GatewayApiClient instance to use for API calls.
+     */
     constructor(gateway?: GatewayApiClient) {
         if (gateway) {
             this.gateway = gateway;
@@ -22,10 +34,34 @@ export class GatewayEzMode {
         }
     }
 
+    /**
+     * Instantiates an account object for the given address.
+     * @param address The Account address of the account to instantiate an object for.
+     * @returns An Account object for the given address.
+     */
     getAccount(address: string): Account {
         return new Account(address, this.gateway);
     }
 
+    /**
+     * Poll the status of a transaction until it is in a 'final' state, either failed or succeeded.
+     * @param transactionId The transaction id / intent hash of the transaction to poll.
+     * @param options Options for polling.
+     * @returns A promise that resolves ∑ith the transaction
+     * status as soon as the transaction is in a final state.
+     *
+     * @example
+     * ```typescript
+     * const transactionId = sendTransaction();
+     * let transactionStatus;
+     * try {
+     *   transactionStatus = await gatewayEzMode.pollTransactionStatus(transactionId);
+     * } catch (error) {
+     *   console.error('Failed polling:', error);
+     * }
+     * console.log("Transaction resolved with status:", transactionStatus);
+     * ```
+     */
     pollTransactionStatus(
         transactionId: string,
         options?: Omit<PollTransactionStatusOptions, 'gatewayApiClient'>
