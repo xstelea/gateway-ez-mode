2cfa1a6f7b31518735063aef0233ee35604d20fc
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-16 17:18:11 +0000

    create a more advanced type-safe metadata extraction helper

--- packages/gateway-ez-mode/src/data_extractors/metadata.ts
+++ packages/gateway-ez-mode/src/data_extractors/metadata.ts
@@ -1,31 +1,57 @@
-import { EntityMetadataCollection } from '@radixdlt/babylon-gateway-api-sdk';
+import {
+    EntityMetadataCollection,
+    MetadataTypedValue,
+} from '@radixdlt/babylon-gateway-api-sdk';
 
-export function extractStringMetadata(
+type GetMetadataValueWithType<T extends MetadataTypedValue['type']> = Extract<
+    MetadataTypedValue,
+    { type: T }
+>;
+
+type OutputTypeForType<T extends MetadataTypedValue['type']> =
+    GetMetadataValueWithType<T> extends {
+        values: infer U;
+    }
+        ? U
+        : GetMetadataValueWithType<T> extends {
+                value: infer U;
+            }
+          ? U
+          : never;
+
+export function extractMetadataValue<T extends MetadataTypedValue['type']>(
     metadataItems: EntityMetadataCollection,
-    key: string
-): string | null {
-    const item = metadataItems.items.find((item) => item.key == key);
+    key: string,
+    type: T
+): OutputTypeForType<T> | null {
+    const item = metadataItems.items.find((item) => item.key === key);
     if (!item) return null;
-    if (item.value.typed.type !== 'String') return null;
-    return item.value.typed.value;
+    if (item.value.typed.type !== type) return null;
+
+    // If it's one of the array kinds, return `values`, else return `value`
+    if ('values' in item.value.typed) {
+        return item.value.typed.values as OutputTypeForType<T>;
+    } else if ('value' in item.value.typed) {
+        return item.value.typed.value as OutputTypeForType<T>;
+    } else {
+        throw new Error(
+            'metadata value should either have `value` or `values`'
+        );
+    }
 }
 
-export function extractUrlMetadata(
-    metadataItems: EntityMetadataCollection,
-    key: string
-): string | null {
-    const item = metadataItems.items.find((item) => item.key == key);
-    if (!item) return null;
-    if (item.value.typed.type !== 'Url') return null;
-    return item.value.typed.value;
-}
+export type MetadataValueDescription = {
+    [key: string]: MetadataTypedValue['type'];
+};
 
-export function extractStringArrayMetadata(
+export function extractAllMetadataValues<T extends MetadataValueDescription>(
     metadataItems: EntityMetadataCollection,
-    key: string
-): string[] | null {
-    const item = metadataItems.items.find((item) => item.key == key);
-    if (!item) return null;
-    if (item.value.typed.type !== 'StringArray') return null;
-    return item.value.typed.values;
+    descriptions: T
+): { [K in keyof T]: OutputTypeForType<T[K]> | null } {
+    return Object.fromEntries(
+        Object.entries(descriptions).map(([key, type]) => [
+            key,
+            extractMetadataValue(metadataItems, key, type),
+        ])
+    ) as { [K in keyof T]: OutputTypeForType<T[K]> | null };
 }
