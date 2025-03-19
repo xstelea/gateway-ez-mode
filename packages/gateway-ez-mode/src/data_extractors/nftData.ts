263aff30f0a8d33fc9e6e91b238a7ece88a8bd71
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-19 21:38:28 +0000

    add some docs to getWithSchema

--- packages/gateway-ez-mode/src/data_extractors/nftData.ts
+++ packages/gateway-ez-mode/src/data_extractors/nftData.ts
@@ -12,6 +12,13 @@ export class SborDataExtractor {
         this.value = value;
     }
 
+    /**
+     * Can be used to extract NFT data. This function takes in a schema for a Scrypto value,
+     * and returns a Result with either the parsed data of that Scrypto value according to the
+     * schema, or an error.
+     * @param schema A fitting sbor-ez-mode schema for the Scrypto value
+     * @returns A Result with either the parsed data or an error
+     */
     public getWithSchema<T>(
         schema: SborSchema<T>
     ): Result<T, SborDataExtractorError> {
